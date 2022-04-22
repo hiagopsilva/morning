b60f1a1bdf47480ff284da45ba50516b4368e203
Hiago Prates <hiagohps360@gmail.com>
2022-04-22 00:37:51 -0300

    feat: days calendar

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -2,12 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React, { FC } from 'react';
 import {
   Avatar,
+  Calendar,
   Content,
+  ContentCalendar,
+  ContentDays,
   Description,
   Header,
+  MonthText,
+  NumberDay,
+  TextDay,
   TextSpan,
   Title,
+  TitleCalendar,
   Wrapper,
+  WrapperDay,
 } from './styles';
 
 type Props = {};
@@ -27,6 +35,22 @@ const Home: FC<Props> = () => {
 
         <Avatar />
       </Header>
+
+      <Calendar>
+        <ContentCalendar>
+          <TitleCalendar>Days</TitleCalendar>
+          <MonthText>Abril</MonthText>
+        </ContentCalendar>
+
+        <ContentDays>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
+            <WrapperDay active={item === 1}>
+              <TextDay active={item === 1}>MON</TextDay>
+              <NumberDay active={item === 1}>{item}</NumberDay>
+            </WrapperDay>
+          ))}
+        </ContentDays>
+      </Calendar>
     </Wrapper>
   );
 };
--- src/scenes/Home/styles.ts
+++ src/scenes/Home/styles.ts
@@ -10,6 +10,7 @@ export const Wrapper = styled.View`
   flex: 1;
   margin-top: ${Platform.OS === 'ios' ? `${getStatusBarHeight() + 8}px` : 0};
   margin-bottom: ${Platform.OS === 'ios' ? `${getBottomSpace()}px` : 0};
+  background: #F9F9F9;
 v`;
 
 export const Header = styled.View`
@@ -45,4 +46,60 @@ export const Avatar = styled.View`
   height: 48px;
   background: #ffffff;
   border-radius: 8px;
+`;
+
+export const Calendar = styled.View`
+  width: 100%;
+
+  margin-top: 26px;
+`;
+
+export const ContentCalendar = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-horizontal: 22px;
+`;
+
+export const TitleCalendar = styled.Text`
+  font-size: 24px;
+  font-weight: 600;
+  color: #3a3c43;
+`;
+
+export const MonthText = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+  color: #3564d8;
+`;
+
+export const ContentDays = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})`
+  padding-horizontal: 22px;
+  margin-top: 14px;
+`;
+
+export const WrapperDay = styled.View`
+  width: 74px;
+  height: 84px;
+  background: ${props => (props.active ? '#7468ef' : '#E5E8EF')};
+  border-radius: 8px;
+
+  align-items: center;
+  justify-content: center;
+
+  margin-right: 17px;
+`;
+
+export const TextDay = styled.Text`
+  color: ${props => (props.active ? '#ffff' : '#B7B0C8')};
+  font-weight: 400;
+  font-size: 18px;
+`;
+
+export const NumberDay = styled.Text`
+  color: ${props => (props.active ? '#ffff' : '#3A3C43')};
+  font-weight: 500;
+  font-size: 24px;
 `;
